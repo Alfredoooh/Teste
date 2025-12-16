56e3c361cd6e7064a08f0069b8f396442c3ec888
Alfredoooh <alfredopjonas@gmail.com>
2025-12-17 00:43:50 +0100

    Atualizar o convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,4 +1,4 @@
-const chromium = require('chrome-aws-lambda');
+const chromium = require('@sparticuz/chromium');
 const puppeteer = require('puppeteer-core');
 
 module.exports = async (req, res) => {
@@ -23,7 +23,7 @@ module.exports = async (req, res) => {
     browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
+      executablePath: await chromium.executablePath(),
       headless: chromium.headless,
     });
 
